18e165c911133bc1de6a9fd8ced528b738e4703b
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2025-01-29 12:18:04 +0530

    Revert "Revert "Initial Commit for rating service""
    This reverts commit eea956d6fef911f118b24b5589aab29f7fecf3e8.

---
+++ RatingService/src/main/java/com/hotel/rating/entities/Rating.java
@@ -0,0 +1,18 @@
+package com.hotel.rating.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Rating {
+
+	@Id
+	private String ratingId;
+	private String userId;
+	private String hotelId;
+	private int rating;
+	private String feedback;
+	
+}
